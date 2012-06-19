1facc23fb843e8e4947bef112207a932a083e721
Philipp Schirmacher <philipp.schirmacher@innoq.com>
2012-06-19 15:29:14 +0200

    added initial-board and basic setup instructions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 chess
 =====
+
+setup
+------------
+
+1. install [leiningen](https://github.com/technomancy/leiningen)
+2. clone this repo, cd into directory
+3. run 'lein deps' to download dependencies that are listed in project.clj
--- src/chess/core.clj
+++ src/chess/core.clj
@@ -1,1 +1,11 @@
 (ns chess.core)
+
+; UPPERCASE -> white
+; lowercase -> black
+(def initial-board
+  {:board (vec (concat
+                (vector [:r :n :b :q :k :b :n :r] (vec (repeat 8 :p)))
+                (repeat 4 (vec (repeat 8 :_)))
+                (vector (vec (repeat 8 :P)) [:R :N :B :Q :K :B :N :R]))),
+   :rochade #{:k :q :K :Q},
+   :turn :w }) 
