15ad7a01182c4868e53f333f2aa6fc2af178fe54
Marcel Winkelkotte <marcel.winkelkotte@gmx.de>
2012-06-19 20:03:11 +0200

    added swank for debugging via swank-cdt

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject demo-app "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dev-dependencies [[swank-clojure "1.4.0"]]
+  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
+  :extra-classpath-dirs ["/usr/lib/jvm/java-6-sun/lib/tools.jar"])
