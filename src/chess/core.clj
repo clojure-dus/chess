70b90a5d6eec1b41afda51d42971a9e5471c20a9
Marcel Winkelkotte <marcel.winkelkotte@gmx.de>
2012-06-20 21:44:36 +0200

    basic movements

--- src/chess/core.clj
+++ src/chess/core.clj
@@ -10,6 +10,13 @@
    :rochade #{:k :q :K :Q},
    :turn :w })
 
+(def white-figures #{ :R :N :B :Q :K :P })
+(def black-figures #{ :r :n :b :q :k :p })
+
+(defn white? [x] (contains? white-figures x))
+
+(defn black? [x] (contains? black-figures x))
+
 (defn figure-at "returns the figure keyword for the given board and coordinates"
  [board x y]
   (get-in board [:board x y]))
@@ -17,13 +24,42 @@
 (defn- set-figure [board x y figure]
   (assoc-in board [:board x y] figure))
 
-(defn- pos-empty? [board x y] (= :_ (figure-at board x y)))
+(defn- pos-empty? "is a position on the given board empty"
+  [board x y] (= :_ (figure-at board x y)))
 
-(defn move-figure "moves a figure for the given board from x1,y2 to x2,y2"
+(defn- pos-on-board? [x y]
+  (every? (fn [n] (and (>= n 0) (<= n 7))) [x y]))
+
+(defn enemy-on-pos? [board x y]
+  (let [ fig-at-pos (figure-at board x y) ] 
+  (if (= :w (:turn board))
+    (black? fig-at-pos)
+    (white? fig-at-pos))))
+                        
+(defn move-figure "moves a figure on the given board from x1,y2 to x2,y2"
   [board x1 y1 x2 y2]
-  {:pre  [(every? (fn [i] (and (> i -1) (< i 8))) [x1 x2 y1 y2])
+  {:pre  [(pos-on-board? x1 y1)
+          (pos-on-board? x2 y2)
           (not (pos-empty? board x1 y1))]}
   (let [fig (figure-at board x1 y1)]
     (set-figure (set-figure board x1 y1 :_) x2 y2 fig)))
 
+(defn moves-vertical
+  [x y c]
+       (partition 2 (interleave c (repeat y))))
 
+(defn moves-down [x y]
+  (moves-vertical x y (range (inc x) 8)))
+
+(defn moves-up [board x y]
+  (moves-vertical x y (range (dec x) -1 -1)))
+
+(defn moves-horizontal
+  [x y c]
+  (partition 2 (interleave (repeat x) c)))
+
+(defn moves-right [x y]
+  (moves-horizontal x y (range (inc y) 8)))
+
+(defn moves-left [x y]
+  (moves-horizontal x y (range (dec y) -1 -1)))
