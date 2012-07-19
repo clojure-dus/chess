d756d495b7f454970e3d770c87e069418a5659df
john <elangie@gmx.de>
2012-07-19 16:20:17 +0200

    added square vector to chessboard
    square helps to index into  the right bitboard per moved picce

--- src/chess/bitboard.clj
+++ src/chess/bitboard.clj
@@ -8,6 +8,7 @@
 (set! *warn-on-reflection* true)
 (use 'clojure.pprint)
 
+(def ^:const NoIdx -1)
 (def ^:const WhitePawnsIdx 0)
 (def ^:const WhiteRooksIdx 1)
 (def ^:const WhiteKnightsIdx 2)
@@ -21,13 +22,26 @@
 (def ^:const BlackQueensIdx 10)
 (def ^:const BlackKingIdx 11)
 
+(let [bit-vec  (loop [bitmasks (vector-of :long 2r1) n 0]
+    (if (= 63 n) bitmasks
+        (recur  (conj bitmasks (bit-shift-left (last bitmasks) 1)) (inc n))))]
+  (defn bitvector ^long [x] (bit-vec x)))
+
 (defrecord ChessBoard [^longs Bitboards
                        ^long  Whitepieces
                        ^long  Blackpieces
-                       ^long  Allpieces])
+                       ^long  Allpieces
+                       squares
+                       ])
 
 (def ^ChessBoard initial-board
-          (ChessBoard.
+  (let [squares (apply (partial vector-of :int)
+                           (concat '(1 2 3 4 5 3 2 1)
+                           (repeat 8 WhitePawnsIdx)
+                           (repeat 32 NoIdx)
+                           (repeat 8 BlackPawnsIdx)
+                           '(7 8 9 10 11 9 8 7)))]
+  (ChessBoard.
              (into-array Long/TYPE [
                (unchecked-long 2r0000000000000000000000000000000000000000000000001111111100000000)
                (unchecked-long 2r0000000000000000000000000000000000000000000000000000000010000001)
@@ -43,32 +57,46 @@
                (unchecked-long 2r0000100000000000000000000000000000000000000000000000000000000000)
                (unchecked-long 2r0001000000000000000000000000000000000000000000000000000000000000)])
 
-              2r0000000000000000000000000000000000000000000000001111111111111111
-              2r1111111111111111000000000000000000000000000000000000000000000000
-              2r1111111111111111000000000000000000000000000000001111111111111111))
+               (unchecked-long 2r0000000000000000000000000000000000000000000000001111111111111111)
+               (unchecked-long 2r1111111111111111000000000000000000000000000000000000000000000000)
+               (unchecked-long 2r1111111111111111000000000000000000000000000000001111111111111111)
 
-  (defn update-bitboard [^ChessBoard board  piece ^long mask]
-    (let [bbs (aclone ^longs (.Bitboards  board))
-          old (aget bbs piece)
-          _ (aset-long bbs piece (bit-xor old mask))
-          whitepieces (bit-or (aget bbs WhitePawnsIdx)
+               squares)))
+
+  (defn update-chessboard [^ChessBoard board  ^long from-square ^long dest-square]
+    (let [bbs  (aclone ^longs (.Bitboards  board))
+          squares (.squares board)
+         
+          from-mask            (bitvector from-square)
+          to-mask              (bitvector dest-square)
+          update-mask          (bit-or from-mask  to-mask)
+          piece-idx            (squares from-square)
+          captured-piece-idx   (squares dest-square)
+          squares              (assoc-in squares [from-square] NoIdx)
+          squares              (assoc-in squares [dest-square] piece-idx)]
+            (aset-long bbs piece-idx (bit-xor (aget bbs piece-idx) update-mask))
+            (when (> -1 captured-piece-idx)
+               (aset-long bbs captured-piece-idx
+                      (bit-xor (aget bbs captured-piece-idx) to-mask)))
+            (let [
+          
+              whitepieces (bit-or (aget bbs WhitePawnsIdx)
                               (aget bbs WhiteRooksIdx)
                               (aget bbs WhiteKnightsIdx)
                               (aget bbs WhiteBishopsIdx)
                               (aget bbs WhiteQueensIdx)
                               (aget bbs WhiteKingIdx))
 
-          blackpieces (bit-or (aget bbs BlackPawnsIdx)
+              blackpieces (bit-or (aget bbs BlackPawnsIdx)
                               (aget bbs BlackRooksIdx)
                               (aget bbs BlackKnightsIdx)
                               (aget bbs BlackBishopsIdx)
                               (aget bbs BlackQueensIdx)
                               (aget bbs BlackKingIdx))
 
-          allpieces (bit-or whitepieces blackpieces)
-          ]
-      (ChessBoard. bbs whitepieces blackpieces allpieces)))
-
+              allpieces (bit-or whitepieces blackpieces)]
+        (ChessBoard. bbs whitepieces blackpieces allpieces squares))))
+   
 (defn flatten-coord [file rank]
   (let [ file (inc file)
          rank (inc rank)]
@@ -77,22 +105,15 @@
 (comment
   (def bitvector (reduce (fn [bm x] (conj bm (expt 2 x))) [] (range 63))))
 
-(let [bit-vec  (loop [bitmasks (vector-of :long 2r1) n 0]
-    (if (= 63 n) bitmasks
-        (recur  (conj bitmasks (bit-shift-left (last bitmasks) 1)) (inc n))))]
-  (defn bitvector ^long [x] (bit-vec x)))
-
 (defn move [^ChessBoard board pieceindex from-cord to-cord]
   (let [ [from-file from-rank] from-cord
          [to-file to-rank]     to-cord
           from-pos             (flatten-coord from-file from-rank)
           to-pos               (flatten-coord to-file to-rank)
-          from-mask            (bitvector from-pos)
-          update-mask          (bit-or from-mask  (bitvector to-pos))
-          ] (update-bitboard board pieceindex update-mask)))
+          0
+          ] (update-chessboard board from-pos to-pos)))
 
 (move initial-board WhitePawnsIdx [0 1] [0 3])
-
 
 
 
